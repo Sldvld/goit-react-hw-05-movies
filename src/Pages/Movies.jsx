2b34b7bae8f05a4fc537e56f0f9780b19c6599ae
Sldvlld <sladvlad@gmail.com>
2023-05-02 22:11:42 +0300

    add pages

---
+++ src/Pages/Movies.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Movies = () => {
+  return <p>Movies</p>;
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,19 @@
+import { Link, Route, Routes, Navigate } from 'react-router-dom';
+import { Home } from 'Pages/Home';
+import { Movies } from 'Pages/Movies';
+
 export const App = () => {
-  return;
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/movies">Movies</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="*" element={<Navigate to="/" replace />}></Route>
+      </Routes>
+    </>
+  );
 };
